3d27f1a993fb3290037835bc577de4fd5ca6ea27
Josephx64 <josephx64@gmail.com>
2014-10-30 16:52:31 -0600

    Versión inicial del proyecto
    Primer commit de los archivos del proyecto.

---
+++ js/widgets.config.js
@@ -0,0 +1,12 @@
+$(document).ready(function(e){
+
+	//Configs for Accordions
+	$(function (){
+		$('.widgetAccordion').accordion({
+			collapsible:false,
+			animate:{duration:900},
+			disabled:false,
+			heightStyle: "fill"
+		});
+	});
+});
